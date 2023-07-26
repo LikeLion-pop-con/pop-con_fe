a14f049b30c5d1e59d6ceeb30f534a866ae423ad
Seongjun <opus706@naver.com>
2023-07-27 01:28:33 +0900

    chore: to save

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Router from "./Router";
 
 import Layout from "./Components/Layout/Layout";
 import Main from "./pages/MainPage/Main";
+import Welcome from "./pages/Welcome/Welcome";
 
 function App() {
+  <GlobalStyle/>
   return (
+    
     <BrowserRouter>
       <Routes>
        <Route element={<Layout />}>
         <Route path='/' element={<Main />} />
+        <Route path='/Welcome' element={<Welcome/>} />
         <Route path='*' element={<div>없는페이지임</div>} />
        </Route>
       </Routes>
     </BrowserRouter>
+  
   );
 }
 
