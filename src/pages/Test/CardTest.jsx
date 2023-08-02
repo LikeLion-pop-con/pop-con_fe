bdfd930c16abee0b56dd982060448d66bd775913
Seongjun <opus706@naver.com>
2023-08-02 21:03:52 +0900

    feat: plus AdminCard component

--- src/pages/Test/CardTest.jsx
+++ src/pages/Test/CardTest.jsx
@@ -9,12 +9,15 @@ import SmallCard from "../../Components/Card/SmallCard";
 import PostCard from "../../Components/Card/PostCard";
 import PopupCard from "../../Components/Card/PopupCard";
 import CheckCard from "../../Components/Card/CheckCard";
+import AdminCard from "../../Components/Card/AdminCard";
+
 
 import Cardrose from "../../assets/Icons/Card/Cardrose.jpg";
 import SmallCardGH from "../../assets/Icons/Card/SmallCardGH.jpg";
 import NewJeans from "../../assets/Icons/Card/NewJeans.jpg";
 import PostCardimg1 from "../../assets/Icons/Card/PostCardimg1.png";
 import PopupCardimg1 from "../../assets/Icons/Card/PopupCardimg1.png";
+import AdminCardimg from "../../assets/Icons/Card/AdminCardimg.png";
 
 import Headerline from "../../Components/Headerline/Headerline";
 import PageTabs from "../../Components/PageTitle/PageTabs";
@@ -63,9 +66,8 @@ const CardTest = () => {
         <PopupCard image={PopupCardimg1} title='IAB studio 팝업 요청되었습니다!' main={'팝업 장소와 일정이 확정되면 알려드릴게요.\n이 팝업 정보를 공유해보세요!'}/>
 
         <CheckCard title='제일 인기가 많은 대전에서 팝업을 열까요?'/>
+        <AdminCard image={AdminCardimg} title='압구정 사거리에 위치한 대형 건물 ' space={'서울 성동구 서울숲2길'} floor='지하 - 층 ~ 지상 1 층' area='연면적 : 62.8,099㎡' />
       </CardBlock>
-
-      <Headerline title='dkssud' subtitle='dksdu' content='dksdu'/>
     </>
   );
 };
