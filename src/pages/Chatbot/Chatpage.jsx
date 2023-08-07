c99b4363333c68584ba614ba99a375ec579ef225
이상혁 <john8538@naver.com>
2023-08-07 17:01:48 +0900

    pull

--- src/pages/Chatbot/Chatpage.jsx
+++ src/pages/Chatbot/Chatpage.jsx
@@ -2,10 +2,19 @@ import React, { useState } from "react";
 import Chatbot from "./Chatbot"; // Chatbot 컴포넌트의 경로에 맞게 수정하세요.
 
 const ChatPage = () => {
+  const [isChatbotOpen, setIsChatbotOpen] = useState(false); // State variable to track Chatbot visibility
+
+  // Function to toggle Chatbot visibility
+  const toggleChatbot = () => {
+    setIsChatbotOpen(!isChatbotOpen);
+  };
 
   return (
     <div>
-        <Chatbot/>
+      <button onClick={toggleChatbot}>Open Chatbot</button>
+
+      {/* Conditionally render the Chatbot */}
+      {isChatbotOpen && <Chatbot setIsChatbotOpen={setIsChatbotOpen} />}
     </div>
   );
 };
